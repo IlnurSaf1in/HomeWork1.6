ee04cb7583f632afb21a85d23a38516ce6498e72
Ильнур <ilnursafin13312@gmail.com>
2022-08-05 10:38:30 +0300

    Исправления домашки

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main {
     }
 
 
+
         System.out.println(allExpensesMonth + " Сумма трат за месяц" );
         float averageExpenses= allExpensesMonth / arr.length;
         System.out.println(averageExpenses + " средние затраты за день");
