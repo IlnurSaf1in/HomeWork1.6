071757965a7be40819d74190b3d2cc7497b93552
Ильнур <ilnursafin13312@gmail.com>
2022-08-05 10:24:12 +0300

    Исправления домашки

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,38 @@
 import java.util.Random;
 
 public class Main {
-    public static int[] day(){
+    public static int[] generateRandomArray(){
         Random random = new Random();
-        int[] days = new int[30];
-        for (int i = 0; i < days.length; i++){
-            days[i] = random.nextInt(100_000) + 100_100;
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = random.nextInt(100_000) + 100_100;
         }
-        return days;
+        return arr;
 
     }
     public static void main(String[] args) {
+        int arr[] = generateRandomArray();
         int allExpensesMonth = 0;
-    for (int i = 0; i < day().length; i++){
-    allExpensesMonth = allExpensesMonth + day()[i];
+    for (int i = 0; i < arr.length; i++){
+    allExpensesMonth = allExpensesMonth + arr[i];
     }
 
 
         System.out.println(allExpensesMonth + " Сумма трат за месяц" );
-        float averageExpenses= allExpensesMonth;
-        System.out.println(averageExpenses / 30 + " трат в день в среднем");
-        int maxExpenses = 0;
-        for (int i = 0; i < day().length; i++) {
-            if (maxExpenses < day()[i]){
-                maxExpenses = day()[i];
+        float averageExpenses= allExpensesMonth / arr.length;
+        System.out.println(averageExpenses + " средние затраты за день");
+        int maxExpenses = arr[0];
+        int minExpenses = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (maxExpenses < arr[i]){
+                maxExpenses = arr[i];
+                if (minExpenses > arr[i]) {
+                    minExpenses = arr[i];
+                }
 
             }
         }
         System.out.println(maxExpenses + " Max zna4enie");
-        int minExpenses = maxExpenses;
-        for (int i = 0; i < day().length; i++) {
-            if (minExpenses > day()[i]) {
-                minExpenses = day()[i];
-            }
-        }
         System.out.println(minExpenses + " min zna4enie");
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         int nameSize = reverseFullName.length ;
